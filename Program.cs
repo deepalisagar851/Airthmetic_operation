fc51e1ea1b210b4f366191dac8b1b018cbcbbd79
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-04-08 13:49:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Airthmetic_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The Addition is: {0}", num1 + num2);
+            Console.WriteLine("The Substraction is: {0}", num1 - num2);
+            Console.WriteLine("The Multiplication is: {0}", num1 * num2);
+            Console.WriteLine("The Division is: {0}", num1 / num2);
+        }
+    }
+}
